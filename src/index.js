a8a7f904111890a9b3da3943da78cb0641be8a76
Harshak777 <Harshak777@users.noreply.github.com>
2020-05-04 10:02:38 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import store from './store';
+import { bugAdded,bugResolved } from './actions';
+
+//const unsubscribe = store.subscribe(()=> {
+//    console.log('Store Changed!!!', store.getState());
+//})
+
+store.dispatch(bugAdded("Bug1"));
+
+//unsubscribe();
+
+store.dispatch(bugResolved(1));
+
+console.log(store.getState())
